4023e7874479c23aa614ba44a4ca8da7cddbecae
midas <arayamidas@gmail.com>
2024-06-03 19:51:07 -0400

    test

---
+++ jueguitos.js
@@ -0,0 +1,16 @@
+module.exports = [
+    {
+        "id":"000",
+        "nombre": "Fortnite: Salvar el Mundo",
+        "genero":"Looter Shooter, Aventura, Defensa de Torres",
+        "precio": "$12.000",
+        "foto":"https://static.wikia.nocookie.net/esmegaman/images/5/5f/MM3ProtoMan.jpg",
+    },
+    {
+        "id":"001",
+        "nombre": "Fortnite: 2",
+        "genero":"La puta que lo parió",
+        "precio": "$100.000",
+        "foto":"https://static.wikia.nocookie.net/esmegaman/images/5/5f/MM3ProtoMan.jpg",
+    },
+];
