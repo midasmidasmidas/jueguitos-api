d386dadb95eff8bf198f9e4d1e81935bf7f09bc4
midas <arayamidas@gmail.com>
2024-06-03 20:57:56 -0400

    y funcionará ahora?

--- jueguitos.js
+++ jueguitos.js
@@ -4,14 +4,21 @@ const jueguitos = [
         "nombre": "Fortnite: Salvar el Mundo",
         "genero":"Looter Shooter, Aventura, Defensa de Torres",
         "precio": "$12.000",
-        "foto":"https://github.com/midasmidasmidas/jueguitos-api/blob/main/imagenes/fortnite_stw.png?raw=true",
+        "foto":"https://i.ibb.co/KKjnD02/fortnite-stw.png",
     },
     {
         "id":1,
         "nombre": "La Leyenda de Pipi",
         "genero":"Aventura, Hack & Slash",
         "precio": "$1.000.000",
-        "foto":"https://github.com/midasmidasmidas/jueguitos-api/blob/main/imagenes/pipi.png?raw=true",
+        "foto":"https://i.ibb.co/vJr7ztb/pipi.png",
+    },
+    {
+        "id":2,
+        "nombre": "Dante's Inferno",
+        "genero":"Acción, Hack & Slash",
+        "precio": "$15.000",
+        "foto":"https://i.ibb.co/H2FgBs9/dantes-inferno.png",
     },
 ];
 
